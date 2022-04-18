c23967a52b0388e46f15d679cf3bbf769c717b21
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-04-18 20:46:25 +0700

    Initial commit

---
+++ DAL/TaiKhoanDAL.cs
@@ -0,0 +1,52 @@
+﻿using QuanLyMyPham.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyMyPham.DAL.InterfaceService;
+namespace QuanLyMyPham.DAL
+{
+    internal class TaiKhoanDAL : ITaiKhoanDAL
+    {
+        private DBContext db = new DBContext();
+        public int Add(TaiKhoan taiKhoan)
+        {
+            db.TaiKhoans.Add(taiKhoan);
+            return db.SaveChanges();
+        }
+
+        public string DangNhap(string TenDangNhap,string MatKhau)
+        {
+            var tk = db.TaiKhoans.FirstOrDefault(x => x.TenDangNhap == TenDangNhap && x.MatKhau == MatKhau);
+            if (tk == null)
+            {
+                return "";
+            }
+
+            return tk.LoaiTK;
+        }
+
+        public int Delete(string id)
+        {
+            var tk = db.TaiKhoans.Find(id);
+            db.TaiKhoans.Remove(tk);
+            return db.SaveChanges();
+        }
+
+        public List<TaiKhoan> GetAll()
+        {
+            return db.TaiKhoans.ToList();
+        }
+
+        public TaiKhoan GetTaiKhoan(string TenDangNhap)
+        {
+            return db.TaiKhoans.Find(TenDangNhap);
+        }
+
+        public int Update(TaiKhoan taiKhoan)
+        {
+           
+        }
+    }
+}
---
+++ Database.sql
@@ -0,0 +1,74 @@
+﻿use master
+go
+create database qlmypham
+go
+use qlmypham
+go
+create table TaiKhoan(
+    TenDangNhap nvarchar(20) primary key ,
+    MatKhau nvarchar(20) not null,
+    LoaiTK nvarchar(10)
+)
+go
+create table LoaiSanPham(
+    MaLoai int identity primary key ,
+    TenLoai nvarchar(50)
+)
+go
+create table DaiLy(
+    MaDL int identity primary key ,
+    TenDL nvarchar(50),
+    SDT char(10),
+    DiaChi nvarchar(100)
+)
+go
+create table NhanVien(
+    MaNV int identity  primary key ,
+    TenNV nvarchar(50),
+    SDT char(10),
+    DiaChi nvarchar(50),
+)
+go
+create table SanPham(
+    MaSP int identity primary key ,
+    TenSP nvarchar(50),
+    MaLoai int not null,
+    NgaySX date,
+    HanSD date,
+    SoLo int,
+    DonGia int
+)
+go
+create table HoaDonBan(
+    MaHD int primary key ,
+    NgayBan date,
+    TenKhach nvarchar(50),
+    SDT char(10),
+    DiaChi nvarchar(100),
+    MaNV int not null,
+    TongTien int
+)
+go
+create table ChiTietHDB(
+    MaHD int not null,
+    MaSP int not null,
+    constraint pk_chitiethdb primary key (MaHD,MaSP),
+    GiaBan int,
+    SoLuong int,
+    ThanhTien int
+)
+go
+create table HoaDonNhap(
+    MaHD int not null primary key ,
+    NgayNhap date,
+    MaDL int,
+    TongTien int
+)
+go
+create table ChiTietHDN(
+    MaHD int not null,
+    MaSP int not null,
+    GiaNhap int,
+    SoLuong int,
+    ThanhTien int
+)
